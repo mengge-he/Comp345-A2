af6900214d7d697f9873afa46ff2ab68635e68e9
mengge-he <125909363+mengge-he@users.noreply.github.com>
2024-11-11 22:01:42 +0000

    add executed value in deploy order

--- Orders.cpp
+++ Orders.cpp
@@ -135,6 +135,7 @@ bool deployOrder::validate() const {
 
 void deployOrder::execute() {
     if (validate()) {
+        *executed=true;
         std::cout<<"Origin armies in target territory:"<<this->target->getArmies()<<"\n";
        this->player->setNumberOfReinforcement(this->player->getNumberOfReinforcement() - this->armies);
         this->target->setArmies(this->target->getArmies() + this->armies);
